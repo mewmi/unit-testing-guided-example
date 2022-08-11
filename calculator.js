659a0752bbf72247973d273329efa16ef4147aed
joao <njoao99@gmail.com>
2022-08-11 17:39:10 +0100

    finished

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function subtract(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
-    throw new Error('cannot divide by 0 idiot');
+    throw new Error('cannot divide by 0');
   } else {
     return a / b;
   }
